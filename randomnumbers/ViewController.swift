dd010870a52f722b0309fe5cdf8adb9275ecfbc0
Korissa. RisakoSuzuki <okasi_aru570@icloud.com>
2023-06-20 00:08:55 +0900

    Initial Commit

---
+++ randomnumbers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  randomnumbers
+//
+//  Created by 鈴木理紗子 on 2023/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ randomnumbers/randomTableView.swift
@@ -0,0 +1,22 @@
+//
+//  randomTableView.swift
+//  randomnumbers
+//
+//  Created by 鈴木理紗子 on 2023/06/20.
+//
+
+import UIKit
+
+class randomTableView: UITableView {
+
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
